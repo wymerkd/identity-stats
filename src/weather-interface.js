069daaf72ae2da3b239928c88b12f760cc4081cb
Student <example@example.com>
2019-11-19 13:55:14 -0800

    add description to js by accessing a more deeply nested key-value pair

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
       $('.showSunset').text(`Todays sunset will be at ${response.sys.sunset}.`);
-      $('.showClouds').text(`Cloud Survey: ${response.weather[0].main}.`);
+      $('.showClouds').text(`Cloud Survey: ${response.weather[0].description}.`);
     }
   });
 });
