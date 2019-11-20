65caed56281462fb8896949ae5df26db17d7b3fe
Student <example@example.com>
2019-11-20 15:50:44 -0800

    add whitepages pro API

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { Status } from './../src/randomuser.js';
+import { Details } from './../src/randomuser.js';
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
 
 $(document).ready(function() {
-
   $('#status-button').click(function() {
+
     (async () => {
       let getStatus = new Status();
       const response = await getStatus.getProfile();
@@ -14,17 +15,32 @@ $(document).ready(function() {
 
     function getElements(response) {
       $('#profilePic').attr("src", response.results[0].picture.large);
-       $('.name').text(`Name: ${response.results[0].name.title} ${response.results[0].name.first} ${response.results[0].name.last}`);
-       $('.age').text(`Age: ${response.results[0].dob.age}`);
-       $('.dob').text(`DOB: ${response.results[0].dob.date}`);
-       $('.social').text(`SSN: ${response.results[0].id.value}`);
-       $('.address').text(`Address: ${response.results[0].location.street.number} ${response.results[0].location.street.name}`);
-       $('.city').text(`City: ${response.results[0].location.city}`);
-       $('.state').text(`State: ${response.results[0].location.state}`);
-       $('.country').text(`Country: ${response.results[0].location.country}`);
-       $('.phone').text(`Phone: ${response.results[0].phone}`)
-       $('.email').text(`Email: ${response.results[0].email}`)
+      $('.name').text(`Name: ${response.results[0].name.title} ${response.results[0].name.first} ${response.results[0].name.last}`);
+      $('.age').text(`Age: ${response.results[0].dob.age}`);
+      $('.dob').text(`DOB: ${response.results[0].dob.date}`);
+      $('.social').text(`SSN: ${response.results[0].id.value}`);
+      $('.address').text(`Address: ${response.results[0].location.street.number} ${response.results[0].location.street.name}`);
+      $('.city').text(`City: ${response.results[0].location.city}`);
+      $('.state').text(`State: ${response.results[0].location.state}`);
+      $('.country').text(`Country: ${response.results[0].location.country}`);
+      $('.phone').text(`Phone: ${response.results[0].phone}`)
+      $('.email').text(`Email: ${response.results[0].email}`)
     }
+  });
 
+
+  $('#detail-button').click(function() {
+    const phone_number = $("#phone_number").val();
+    const country_code = $("#country_code").val();
+
+    (async () => {
+      let getDetails = new Details();
+      const detail = await getDetails.getProfileAgain(country_code, phone_number );
+      getElementsAgain(detail);
+    })();
+
+    function getElementsAgain(detail) {
+      $('.associates').text(`Associates: ${detail.belongs_to[0].name}`);
+    }
   });
 });
--- src/randomuser.js
+++ src/randomuser.js
@@ -9,3 +9,16 @@ export class Status {
     }
   }
 }
+
+
+export class Details {
+  async getProfileAgain(country_code, phone_number) {
+    try {
+      let response = await fetch(`https://api.ekata.com/3.0/phone.json?api_key=17021b71bff2492aad8b53285ba67c54&phone.country_hint=${country_code}&phone=${phone_number}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
+  }
+}
